b990527362a4faa5c446b00e43d3277d0133ece2
Abhay vishwakarma <gaamagaming@gmail.com>
2024-03-26 14:44:04 +0530

    blog responcive updated

--- src/routs/Blogs.jsx
+++ src/routs/Blogs.jsx
@@ -5,52 +5,52 @@ import { Link } from 'react-router-dom'
 import Img from '../components/img'
 
 function Blogs() {
-  const [apis, setAPIs] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [apis, setAPIs] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetchAPIs();
-  }, []);
+  // useEffect(() => {
+  //   fetchAPIs();
+  // }, []);
 
-  const fetchAPIs = async () => {
-    try {
-      const response = await fetch('https://api.publicapis.org/entries');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await response.json();
-      setAPIs(data.entries);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const fetchAPIs = async () => {
+  //   try {
+  //     const response = await fetch('https://api.publicapis.org/entries');
+  //     if (!response.ok) {
+  //       throw new Error('Failed to fetch data');
+  //     }
+  //     const data = await response.json();
+  //     setAPIs(data.entries);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  // if (error) {
+  //   return <div>Error: {error}</div>;
+  // }
 
   return (
     <div>
      <Nav/>
-     <div className='h-[80vh] w-full flex items-center justify-center'>
+     <div className='sm:h-[80vh] mt-7 w-full pb-7 flex items-center justify-center'>
       <Img/>
-        <div className='h-[400px] w-[85vw] border border-zinc-600 overflow-hidden rounded-3xl flex '>
-          <div className='w-1/2 h-full flex flex-col items-center'>
-            <p className='w-full px-20 mt-16 text-[0.8rem] font-medium'>March 25, 2024</p>
-            <h1 className='text-3xl px-16 font-semibold'>Amazon Advertising Simplified: Maximising Your Marketing Potential</h1>
-            <p className='w-full px-16 mb-10 mt-2'>Content Team</p>
-            <p className='text-start px-16'>Discover the developments in Amazon advertising services to create impactful campaigns that will distinguish your brand from</p>
-            <Link to="../error" className='w-full px-20 text-red-400 font-medium'>Read More...</Link>
+        <div className='sm:h-[400px] w-[85vw] border border-zinc-600 overflow-hidden rounded-3xl blogpgcont sm:flex '>
+          <div className='sm:w-1/2 bolgpgfrstdv h-full flex flex-col items-center'>
+            <p className='w-full sm:px-20 mt-16 text-[0.8rem] font-medium'>March 25, 2024</p>
+            <h1 className='text-3xl sm:px-16 font-semibold'>Amazon Advertising Simplified: Maximising Your Marketing Potential</h1>
+            <p className='w-full sm:px-16 mb-10 mt-2'>Content Team</p>
+            <p className='text-start sm:px-16'>Discover the developments in Amazon advertising services to create impactful campaigns that will distinguish your brand from</p>
+            <Link to="../error" className='w-full sm:px-20 text-red-400 font-medium'>Read More...</Link>
           </div>
-          <div className='w-1/2 h-full flex items-center justify-center'>
-            <img className='h-[320px] rounded-2xl' src="https://www.gonukkad.com/wp-content/uploads/2024/03/Amazon-Advertising.webp" />
+          <div className='sm:w-1/2 p-3 h-full flex items-center justify-center'>
+            <img className='h-[320px] object-cover rounded-2xl' src="https://www.gonukkad.com/wp-content/uploads/2024/03/Amazon-Advertising.webp" />
           </div>
         </div>
      </div>
