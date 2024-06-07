656225c93f2944b24b4dffce196ae28c67dcdf94
Abhay Vishwakarma <gaamaofficial@gmail.com>
2024-06-08 00:20:12 +0530

    Update navunder.jsx

--- src/components/navunder.jsx
+++ src/components/navunder.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Navunder() {
   return (
     <div className='w-80 p-5 h-fit flex items-center flex-col bg-[#5e4fbe] rounded-xl mt-2'>
-          <ul className='flex flex-col items-center w-full font-medium tracking-wide'>
+          <ul className='flex flex-col items-center w-full font-medium tracking-wide text-zinc-100'>
                <Link className='w-full transition-all mt-1'><li className='hover:bg-[#6F5CE3] rounded-lg hover:text-zinc-100 py-1 px-3 w-full text-center'>Ecommerce Seller Support</li></Link>
                <Link className='w-full transition-all mt-1'><li className='hover:bg-[#6F5CE3] rounded-lg hover:text-zinc-100 py-1 px-3 w-full text-center'>Full Acount Management</li></Link>
                <Link className='w-full transition-all mt-1'><li className='hover:bg-[#6F5CE3] rounded-lg hover:text-zinc-100 py-1 px-3 w-full text-center'>Listing & Cataloging</li></Link>
